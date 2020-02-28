81e3e239f00488e45d844ae1460d10354263ed4f
unknown <bwardwell79@gmail.com>
2020-02-28 11:14:30 -0800

    Sprint Submission

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,16 +1,32 @@
 import React, { useEffect, useState } from "react";
+import CharacterCard from './CharacterCard'
+import axios from 'axios';
+import SearchForm from './SearchForm'
 
 export default function CharacterList() {
   // TODO: Add useState to track data from useEffect
+  const [characterInfo, setCharacterInfo] = useState([]);
+
+  const addNewCharacter = newChar => {
+    setCharacterInfo(...characterInfo, newChar)
+  }
 
   useEffect(() => {
     // TODO: Add API Request here - must run in `useEffect`
     //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
+    axios.get('https://cors-anywhere.herokuapp.com/https://rickandmortyapi.com/api/character/')
+    .then((response) => {
+      
+      setCharacterInfo(response.data.results)
+      
+    }).catch((error)=> {console.log(error)})
   }, []);
 
   return (
     <section className="character-list">
-      <h2>TODO: `array.map()` over your state here!</h2>
+      <SearchForm/>
+      <h2>{characterInfo.map(character => {return  <CharacterCard name={character.name}/>
+      })}   </h2>
     </section>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,16 @@
 import React from "react";
+import { Route, Link } from 'react-router-dom';
 
 export default function Header() {
   return (
     <header className="ui centered">
       <h1 className="ui center">Rick &amp; Morty Fan Page</h1>
+      <button>
+        <Link to ='/'>WelcomePage</Link>
+      </button>
+      <button>
+        <Link to ='/CharacterList'>Characters</Link>
+      </button>
     </header>
   );
 }
